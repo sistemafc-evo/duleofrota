aec130f50f2513de6480040cb6d8554070e1fe88
sistemafc-evo <sistemafc.evo@gmail.com>
2026-03-19 21:57:01 -0300

    Update 0_config_firebaseprod.js

--- script/0_config_firebaseprod.js
+++ script/0_config_firebaseprod.js
@@ -1,11 +1,11 @@
 // Configuração do Firebase de Produção
 const firebaseConfig = {
-    apiKey: "AIzaSyBfuqxgjW2KmK9t66-v_Z0SqRuXNB1sYo0",
-    authDomain: "frota-caminhao-producao.firebaseapp.com",
-    projectId: "frota-caminhao-producao",
-    storageBucket: "frota-caminhao-producao.firebasestorage.app",
-    messagingSenderId: "470546136795",
-    appId: "1:470546136795:web:0beaf096dcf1cfacb2d6fe"
+    apiKey: "AIzaSyCr6kaqGWnwGzvExRdVF0NwEaV1D_1BwIE",
+    authDomain: "duleo-frota-producao.firebaseapp.com",
+    projectId: "duleo-frota-producao",
+    storageBucket: "duleo-frota-producao.firebasestorage.app"",
+    messagingSenderId: "1052422226412",
+    appId: "1:1052422226412:web:928b8f179030f52ec8c0c9"
 };
 
 // Inicializar Firebase (verificar se já não foi inicializado)
