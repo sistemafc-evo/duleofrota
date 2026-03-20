9d896bf6e112d244dfc5d26641f3f4968d102ee6
sistemafc-evo <sistemafc.evo@gmail.com>
2026-03-19 22:13:04 -0300

    Update 0_config_firebaseprod.js

--- script/0_config_firebaseprod.js
+++ script/0_config_firebaseprod.js
@@ -8,21 +8,25 @@ const firebaseConfig = {
     appId: "1:1052422226412:web:928b8f179030f52ec8c0c9"
 };
 
-// Inicializar Firebase (verificar se já não foi inicializado)
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+// Inicializar Firebase APÓS verificar se o Firebase está disponível
+if (typeof firebase !== 'undefined' && firebase.apps) {
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    }
+    
+    const db = firebase.firestore();
+    
+    // Aplicar configurações com tratamento de erro
+    db.settings({
+        timestampsInSnapshots: true,
+        ignoreUndefinedProperties: true
+    }).catch(error => {
+        // Ignora erro se as configurações já foram aplicadas
+        console.log("Configurações do Firestore já definidas");
+    });
+    
+    // Tornar db global
+    window.db = db;
+} else {
+    console.error("Firebase não foi carregado corretamente");
 }
-
-const db = firebase.firestore();
-
-// Aplicar configurações com tratamento de erro
-db.settings({
-    timestampsInSnapshots: true,
-    ignoreUndefinedProperties: true
-}).catch(error => {
-    // Ignora erro se as configurações já foram aplicadas
-    console.log("Configurações do Firestore já definidas");
-});
-
-// Tornar db global
-window.db = db;
